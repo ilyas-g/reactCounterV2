477a3a86d33b6e9a26e6985ec76646838894d352
Roddoark <ilyas150691@hotmail.fr>
2022-07-22 16:50:08 +0200

    Correction ET SIMPLIFICATION du code

--- src/App.js
+++ src/App.js
@@ -3,49 +3,41 @@ import './App.css';
 
 function App() {
 
-  const [tabCounter, setTabCounter] = useState([]);
-
-  const divCounter = (
-    <div className='divCounter w-100'>
-      <div className='d-flex mb-15'>
-        <button onClick={() => {
-          console.log('cest moins');
-        }}>-</button>
-        <p>2</p>
-        <button onClick={() => {
-          console.log('cest moins');
-        }}>+</button>
-      </div>
-      <button>Reset</button>
-    </div >
-  );
+  const [tabCounter, setTabCounter] = useState([0]);
 
   const test = () => {
     const newTabCounter = [...tabCounter];
-    newTabCounter.push(divCounter);
+    newTabCounter.push(0);
     setTabCounter(newTabCounter);
-    // setUsers(["Tom", "Gwendoline", "Alexis"]);
     console.log(setTabCounter);
   };
-  const addBtn = (
-    <button
-      className='mb-25'
-      onClick={test}
-    >
-      Add counter
-    </button>
-  );
-
 
   return (
     <div className="App">
       <header>
         <h1>React Counter v2</h1>
       </header>
-      {tabCounter.length < 3 ? addBtn : ''}
+      {tabCounter.length < 3 ?
+        <button
+          className='mb-25'
+          onClick={test}
+        >
+          Add counter
+        </button> : ''}
 
-      {tabCounter.map((user, index) => {
-        return <div className='mb-50' key={index}>{user}</div>;
+      {tabCounter.map((counter, index) => {
+        return <div className='divCounter w-100'>
+          <div className='d-flex mb-15'>
+            <button onClick={() => {
+              console.log('cest moins');
+            }}>-</button>
+            <p>{counter}</p>
+            <button onClick={() => {
+              console.log('cest moins');
+            }}>+</button>
+          </div>
+          <button>Reset</button>
+        </div >;
       })}
 
     </div >
