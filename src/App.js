566e2d5889993db09abbfe6967ea79e1d90d4615
Roddoark <ilyas150691@hotmail.fr>
2022-07-22 15:11:51 +0200

    Some CSS

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ function App() {
   const [tabCounter, setTabCounter] = useState([]);
 
   const divCounter = (
-    <div>
-      <div>
-        <button>-</button>
+    <div className='divCounter w-100'>
+      <div className='d-flex mb-15'>
+        <button onClick={() => {
+          console.log('cest moins');
+        }}>-</button>
         <p>2</p>
-        <button>+</button>
+        <button onClick={() => {
+          console.log('cest moins');
+        }}>+</button>
       </div>
       <button>Reset</button>
     </div >
@@ -23,21 +27,28 @@ function App() {
     // setUsers(["Tom", "Gwendoline", "Alexis"]);
     console.log(setTabCounter);
   };
-  const addBtn = (<button onClick={test}>Add counter</button>);
+  const addBtn = (
+    <button
+      className='mb-25'
+      onClick={test}
+    >
+      Add counter
+    </button>
+  );
+
 
   return (
     <div className="App">
       <header>
         <h1>React Counter v2</h1>
       </header>
-      <div>
-        {tabCounter.length < 3 ? addBtn : ''}
+      {tabCounter.length < 3 ? addBtn : ''}
 
-        {tabCounter.map((user, index) => {
-          return <h1 key={index}>{user}</h1>;
-        })}
-      </div>
-    </div>
+      {tabCounter.map((user, index) => {
+        return <div className='mb-50' key={index}>{user}</div>;
+      })}
+
+    </div >
   );
 }
 
