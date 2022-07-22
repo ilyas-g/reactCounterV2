ade7b433a283be068862b33df81dcc47b2395302
Roddoark <ilyas150691@hotmail.fr>
2022-07-22 13:49:02 +0200

    First commit with first integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>React Counter v2</h1>
       </header>
+      <div>
+        <button>Add counter</button>
+        <div>
+          <button>-</button>
+          <p>2</p>
+          <button>+</button>
+        </div>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
